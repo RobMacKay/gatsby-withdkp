311eadedd54cdf9b052182d0aebf28d7aa93e1ca
Rob Mackay <robskiwarrior@gmail.com>
2019-01-08 22:15:25 +0000

    First Push

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import logo from "../assets/images/website-icon.png"
 
 class Header extends React.Component {
     render() {
         return (
             <section id="header">
                 <div className="inner">
-                    <span className="icon major fa-cloud"></span>
+                    <img src={logo} width="500" />
                     <h1>Hi, I'm <strong>Photon</strong>, another fine<br />
                     little freebie from <a href="http://html5up.net">HTML5 UP</a>.</h1>
                     <p>Accumsan feugiat mi commodo erat lorem ipsum, sed magna<br />
