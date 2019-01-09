a3f1a28192925619002b477d4fee6563b4cb4c08
Rob MacKay <robskiwarrior@gmail.com>
2019-01-09 14:34:00 +0000

    GO

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Photon",
-    author: "Hunter Chang",
-    description: "A Gatsby.js Starter based on Photon by HTML5 UP"
+    title: "WithDKP",
+    author: "Rob MacKay",
+    description: "A great place to find awesome loot"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
